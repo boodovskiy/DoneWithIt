1ffe5d7671a0e9dc7f8b928f8052a740f8d31d43
boodovskiy <dzak1987@mail.ru>
2025-05-27 10:32:06 +0300

    Configure flex settings

--- App.js
+++ App.js
@@ -1,65 +1,33 @@
-import { StyleSheet, 
-    Text, 
-    Image, 
-    TouchableWithoutFeedback, 
-    Button, 
-    Alert, 
-    SafeAreaView, 
-    Platform, 
-    StatusBar, 
-    View,
-    useWindowDimensions } from 'react-native';
- import { useDeviceOrientation } from '@react-native-community/hooks'; 
+import { View } from 'react-native';
 
 export default function App() {
-  const handlePress = () => {
-    console.log('image clicked!');
-  };
 
-  const {landscape} = useDeviceOrientation();
-  const { width, height } = useWindowDimensions();
 
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
-
-      <Text numberOfLines={1}>Hello React Native!</Text>
-
-      <TouchableWithoutFeedback onPress={handlePress}>
-        <Image 
-          source={{ 
-            uri: 'https://picsum.photos/200/300',
-            width: 200,
-            height: 300,
-          }} 
-        />
-      </TouchableWithoutFeedback>
-
-      <Button 
-        color="#841584"
-        title='Click me' 
-        onPress={() => Alert.alert("Button Pressed!", "You clicked the button!", [
-          { text: 'OK' },
-          { text: 'CANCEL' },
-        ])} 
-      />
-
       <View style={{
-        backgroundColor: 'dodgerblue',
-        width: "100%",
-        height: landscape ?  "100%" :  "30%",
-        marginTop: 20,
+        backgroundColor: '#fff',
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}>
+        <View style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+        }} />
+        <View style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 100,
+        }} />
+        <View style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+        }} />
 
       </View>
-    </SafeAreaView>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0, // Adjust for Android status bar height
-  },
-});
