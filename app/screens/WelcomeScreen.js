37fa98a352f6a2c3f6aeef6c037270a00f77a103
boodovskiy <dzak1987@mail.ru>
2025-05-28 10:03:15 +0300

    Building Welcome Screen

---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+
+function WelcomeScreen(props) {
+    return (
+        <ImageBackground
+            style={styles.background}
+            source={require('../assets/background.jpg')}
+        >   
+            <View style={styles.logoContainer}>
+                <Image source={require('../assets/logo.jpg')} style={styles.logo}/> 
+                <Text>Sell What You Don't Need.</Text>
+            </View>
+            <View style={styles.loginButton} />
+            <View style={styles.registerButton} />
+        </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    loginButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: '#fc5c65',
+        borderRadius: 35,
+    },
+    logo: {
+        width: 100,
+        height: 100,
+        borderRadius: 35,
+    },
+    logoContainer: {
+        position: 'absolute',
+        top: 70,
+        alignItems: 'center',
+    },
+    registerButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: '#4ecdc4',
+        borderRadius: 35,
+    },
+})
+
+export default WelcomeScreen;
