1558b6b75a97d5628d525c61042f23772341d35d
boodovskiy <dzak1987@mail.ru>
2025-06-04 09:22:45 +0300

    Add material icons for ViewImageScreen

--- App.js
+++ App.js
@@ -8,5 +8,5 @@ import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 export default function App() {
 
 
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 }
--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Image, StyleSheet, View } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import colors from '../config/colors';
 
 function ViewImageScreen(props) {
     return (
         <View style={styles.container}>
-            <View style={styles.closeIcon} />
-            <View style={styles.deleteIcon} />
+            <View style={styles.closeIcon}><MaterialCommunityIcons name='close' color="white" size={30} /></View>
+            <View style={styles.deleteIcon}><MaterialCommunityIcons  name='trash-can-outline' color="white" size={35}/></View>
             <Image
                 style={styles.image}
                 source={require('../assets/chair.jpg')}
@@ -22,9 +23,6 @@ const styles = StyleSheet.create({
         position: 'absolute',
         top: 40,
         left: 30,
-        width: 50,
-        height: 50,
-        backgroundColor: colors.primary,
         borderRadius: 25,
     },
     container: {
@@ -35,9 +33,6 @@ const styles = StyleSheet.create({
         position: 'absolute',
         top: 40,
         right: 30,
-        width: 50,
-        height: 50,
-        backgroundColor: colors.secondary,
         borderRadius: 25,
     },
     image: {
