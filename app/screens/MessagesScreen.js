acfd89010189bf97fc1bbeffcf68c0de92a429a7
boodovskiy <dzak1987@mail.ru>
2025-06-05 09:16:46 +0300

    Add Separator component

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,7 +1,7 @@
-import React from 'react';
 import { FlatList, StyleSheet } from 'react-native';
 import ListItem from '../components/ListItem';
 import Screen from '../components/Screen';
+import ListItemSeparator from '../components/ListItemSeparator';
 
 const messages = [
     { id: 1, title: 'Message 1', description: 'This is the first message', image: require('../assets/alexbud.jpg') },
@@ -16,6 +16,7 @@ function MessagesScreen(props) {
                 data={messages}
                 keyExtractor={(message) => message.id.toString()}
                 renderItem={({ item }) => <ListItem title={item.title} subTitle={item.description} image={item.image} />}
+                ItemSeparatorComponent={() => <ListItemSeparator />}
             />
         </Screen>
     );
