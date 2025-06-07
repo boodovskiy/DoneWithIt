1648d06d004255b75d274d32acea8d22e5b61d61
boodovskiy <dzak1987@mail.ru>
2025-06-07 11:02:19 +0300

    Deleting an Item

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,16 +1,25 @@
-import { FlatList, StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, StyleSheet } from 'react-native';
 import ListItem from '../components/ListItem';
 import Screen from '../components/Screen';
 import ListItemSeparator from '../components/ListItemSeparator';
 import ListItemDeleteAction from '../components/ListItemDeleteAction';
 
-const messages = [
+const initialMessages = [
     { id: 1, title: 'Message 1', description: 'This is the first message', image: require('../assets/alexbud.jpg') },
     { id: 2, title: 'Message 2', description: 'This is the second message', image: require('../assets/alexbud.jpg') },
     { id: 3, title: 'Message 3', description: 'This is the third message', image: require('../assets/alexbud.jpg') },
 ];
 
 function MessagesScreen(props) {
+    const [messages, setMessages] = useState(initialMessages);
+
+    const handleDelete = (message) => {
+        // Logic to delete the message
+        const newMessages = messages.filter((m) => m.id !== message.id);
+        setMessages(newMessages);
+    }
+    
     return (
         <Screen>
             <FlatList 
@@ -21,7 +30,9 @@ function MessagesScreen(props) {
                     subTitle={item.description} 
                     image={item.image}
                     onPress={() => console.log('Message selected', item.id)}
-                    renderRightActions={ListItemDeleteAction}
+                    renderRightActions={() => 
+                        <ListItemDeleteAction onPress={() => handleDelete(item)}/>
+                    }
                 />}
                 ItemSeparatorComponent={() => <ListItemSeparator />}
             />
