4c08894c393922959a4fd7011daec7bfc01bac80
boodovskiy <dzak1987@mail.ru>
2025-06-08 11:21:14 +0300

    Build the icon component

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
-import { View } from 'react-native';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import AppButton from './app/components/AppButton';
-import Card from './app/components/Card';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import Screen from './app/components/Screen';
+import Icon from './app/components/Icon';
 
 export default function App() {
-
-
-  return <GestureHandlerRootView style={{ flex: 1 }}>
-    <MessagesScreen />
-  </GestureHandlerRootView>;
+  return (
+      <Screen>
+        <Icon 
+          name="email"
+          size={50}
+          backgroundColor="red"
+          iconColor="white"
+        />
+      </Screen>
+    );
 }
