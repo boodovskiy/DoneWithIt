592c69404ec3b8ed0c4446a83fbde6b4696d9082
boodovskiy <dzak1987@mail.ru>
2025-06-11 08:57:37 +0300

    Building the Listing Screen

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import Icon from './app/components/Icon';
 import ListItem from './app/components/ListItem';
 import { Gesture, GestureHandlerRootView } from 'react-native-gesture-handler';
 import AccountScreen from './app/screens/AccountScreen';
+import ListingScreen from './app/screens/ListingScreen';
 
 export default function App() {
   return (
       <GestureHandlerRootView style={{ flex: 1 }}>
-        <AccountScreen />
+        <ListingScreen />
       </GestureHandlerRootView>
     );
 }
