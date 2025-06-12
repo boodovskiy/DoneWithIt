710ba7c58571d56a08184e1e24b88c769c0968f8
boodovskiy <dzak1987@mail.ru>
2025-06-12 11:23:56 +0300

    Build Custom AppTextInput

--- App.js
+++ App.js
@@ -4,11 +4,16 @@ import ListItem from './app/components/ListItem';
 import { Gesture, GestureHandlerRootView } from 'react-native-gesture-handler';
 import AccountScreen from './app/screens/AccountScreen';
 import ListingScreen from './app/screens/ListingScreen';
+import { Text, TextInput } from 'react-native';
+import { useState } from 'react';
+import AppTextInput from './app/components/AppTextInput';
 
 export default function App() {
+  const [firstName, setFirstName] = useState('');
+
   return (
       <GestureHandlerRootView style={{ flex: 1 }}>
-        <ListingScreen />
+        <AppTextInput placeholder='Username' icon={'email'}/>
       </GestureHandlerRootView>
     );
 }
