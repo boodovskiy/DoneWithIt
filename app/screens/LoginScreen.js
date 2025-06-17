74c5e84358e7fea88e5d41a97d7f8d6c7a5a068c
boodovskiy <dzak1987@mail.ru>
2025-06-17 10:30:08 +0300

    Install and set up Formik

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,45 +1,53 @@
-import { useState } from 'react';
 import { Image, StyleSheet } from 'react-native';
 import Screen from '../components/Screen';
 import AppTextInput from '../components/AppTextInput';
 import AppButton from '../components/AppButton';
+import { Formik } from 'formik';
 
 function LoginScreen(props) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
 
     return (
         <Screen style={styles.continuer}>
             <Image 
                 style={styles.logo}
                 source={require('../assets/logo.jpg')}/>
-            <AppTextInput
-                autoCapitalize="none"
-                autoCorrect={false}
-                icon="email"
-                onChangeText={text => setEmail(text)}
-                placeholder="Email"
-                keyboardType="email-address"
-                textContentType="emailAddress"
-            />
-            <AppTextInput
-                autoCapitalize="none"
-                autoCorrect={false}
-                icon="lock"
-                onChangeText={text => setPassword(text)}
-                placeholder="Password"
-                secureTextEntry
-                textContentType="password"
-            />
-            <AppButton
-                title="Login"
-                onPress={() => console.log("Login pressed")}
-            />
-            <AppButton
-                title="Register"
-                onPress={() => console.log("Register pressed")}
-                color="secondary"
-            />
+            <Formik
+                initialValues={{ email: '', password: '' }}
+                onSubmit={values => console.log(values)}
+                validationSchema={null} // Add your validation schema here
+            >
+                {({ handleChange, handleSubmit }) => (
+                    <>
+                        <AppTextInput
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            icon="email"
+                            onChangeText={handleChange('email')}
+                            placeholder="Email"
+                            keyboardType="email-address"
+                            textContentType="emailAddress"
+                        />
+                        <AppTextInput
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            icon="lock"
+                            onChangeText={handleChange('password')}
+                            placeholder="Password"
+                            secureTextEntry
+                            textContentType="password"
+                        />
+                        <AppButton
+                            title="Login"
+                            onPress={handleSubmit}
+                        />
+                        <AppButton
+                            title="Register"
+                            onPress={() => console.log("Register pressed")}
+                            color="secondary"
+                        />
+                    </>
+                )}
+            </Formik>
         </Screen>
     );
 }
