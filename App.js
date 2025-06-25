66e1afcb4d63cdf6f6b6dc656563ba92986bf784
boodovskiy <dzak1987@mail.ru>
2025-06-25 10:18:29 +0300

    Accessing Image Library

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import * as ImagePicker from "expo-image-picker";
+import { View, Image } from "react-native";
+import { Button } from "react-native";
 
 export default function App() {
+  const [imageUri, setImageUri] = useState();
+
   const requestPermission = async () => {
-    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
-    if (!granted) {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status !== "granted") {
       alert("Permission to access camera roll is required!");
     }
   };
@@ -14,5 +18,24 @@ export default function App() {
     requestPermission();
   }, []);
 
-  return <GestureHandlerRootView style={{ flex: 1 }}></GestureHandlerRootView>;
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+
+      if (!result.canceled) {
+        setImageUri(result.assets[0].uri);
+      }
+    } catch (error) {
+      console.error("Error selecting image:", error);
+    }
+  };
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={{ alignItems: "center", marginTop: 50 }}>
+        <Button title="Select Image" onPress={selectImage} />
+        <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+      </View>
+    </GestureHandlerRootView>
+  );
 }
