0e3b58842c1a346716d0b1be050cbaa00964fe7e
boodovskiy <dzak1987@mail.ru>
2025-06-26 09:23:42 +0300

    Build ImageTouches

--- App.js
+++ App.js
@@ -8,35 +8,13 @@ import ImageInput from "./app/components/ImageInput";
 export default function App() {
   const [imageUri, setImageUri] = useState();
 
-  const requestPermission = async () => {
-    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (status !== "granted") {
-      alert("Permission to access camera roll is required!");
-    }
-  };
-
-  useEffect(() => {
-    requestPermission();
-  }, []);
-
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync();
-
-      if (!result.canceled) {
-        setImageUri(result.assets[0].uri);
-      }
-    } catch (error) {
-      console.error("Error selecting image:", error);
-    }
-  };
-
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <View style={{ alignItems: "center", marginTop: 50 }}>
-        <Button title="Select Image" onPress={selectImage} />
-        <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
-        <ImageInput imageUri={imageUri} />
+        <ImageInput
+          imageUri={imageUri}
+          onChangeImage={(uri) => setImageUri(uri)}
+        />
       </View>
     </GestureHandlerRootView>
   );
--- app/components/ImageInput.js
+++ app/components/ImageInput.js
@@ -1,16 +1,65 @@
-import { View, StyleSheet, Image } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Image,
+  TouchableWithoutFeedback,
+  Alert,
+} from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
 
 import colors from "../config/colors";
+import { useEffect } from "react";
 
-function ImageInput({ imageUri }) {
+function ImageInput({ imageUri, onChangeImage }) {
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const requestPermission = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status !== "granted") {
+      alert("Permission to access camera roll is required!");
+    }
+  };
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 0.5,
+      });
+
+      if (!result.canceled) {
+        onChangeImage(result.assets[0].uri);
+      }
+    } catch (error) {
+      console.error("Error selecting image:", error);
+    }
+  };
+
+  const handlePress = () => {
+    if (!imageUri) selectImage();
+    else
+      Alert.alert("Delete", "Are you sure you want to delete this image?", [
+        { text: "Yes", onPress: () => onChangeImage(null) },
+        { text: "No" },
+      ]);
+  };
+
   return (
-    <View style={styles.container}>
-      {!imageUri && (
-        <MaterialCommunityIcons name="camera" size={40} color={colors.medium} />
-      )}
-      {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
-    </View>
+    <TouchableWithoutFeedback onPress={handlePress}>
+      <View style={styles.container}>
+        {!imageUri && (
+          <MaterialCommunityIcons
+            name="camera"
+            size={40}
+            color={colors.medium}
+          />
+        )}
+        {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
